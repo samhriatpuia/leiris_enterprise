d31a8f40065fa58b2a96f4e95bb25cfd22794b50
vullai <samhriatpuia42@gmail.com>
2023-09-04 12:04:33 +0530

    Update Invoice.vue

--- resources/js/Pages/Sales/Invoice.vue
+++ resources/js/Pages/Sales/Invoice.vue
@@ -196,7 +196,7 @@ function destroy(id) {
                                 <div class="grid grid-cols-4 md:grid-cols-3 gap-1">
                                     <div>
                                         <button type="submit" class="mb-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                            Add
+                                            Save
                                         </button>
                                     </div>
                                     <div @click="openModal" class="mb-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
